3ef6ffab7a992d6ff626ea4e0531793b3a1f9a34
prathmesh-utpat <prathmeshutpat88@gmail.com>
2025-01-20 17:24:20 +0530

    Datatypes.cpp

---
+++ Datatypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int age =25; // intiger - 4 bytes 
+    
+     cout <<"age= " << age << endl;
+
+    bool max=25.99 ; // boolean - 4 bytes 
+
+     cout << "max= " << max << endl;
+
+    float salary=99.99; // flat- 4 bytes 
+
+    cout << "salary= " << salary << endl; 
+
+    double income = 99.99; // double -8 bytes 
+
+    cout << "income= " << income << endl;
+
+    char grade = 'p'; // character - 1 bytes 
+
+    cout << grade << endl;
+
+    return 0;
+}
